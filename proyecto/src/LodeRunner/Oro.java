bd2ef227fa4a88e0b2a8cb6c428dbd4a3fa6d6ad
tomasruiz23 <dayyraal@gmail.com>
2026-05-22 16:40:18 -0300

    Cree los componentes de Lode Runner

---
+++ proyecto/src/LodeRunner/Oro.java
@@ -0,0 +1,12 @@
+public class Oro extends Entidad{
+    private int valor;
+
+    public Oro(int x, int y, int ancho, int alto, int valor) {
+        super(x, y, ancho, alto);
+        this.valor = valor;
+    }
+
+    public int obtenerValor() {
+        return valor;
+    }
+}
