68ee56720d91862efe295e49fa9973053a02ed68
brutom26@gmail.com <brutom26@gmail.com>
2026-05-22 01:59:59 -0300

    ahora si subo codigo Jugador.java

---
+++ proyecto/src/motor/Jugador.java
@@ -0,0 +1,22 @@
+package motor;
+
+public abstract class Jugador extends Entidad {
+    protected int vidas;
+
+    public Jugador(int x, int y, int ancho, int alto, int vidas) {
+        super(x, y, ancho, alto);
+        this.vidas = vidas;
+    }
+
+    public int obtenerVidas() {
+        return vidas;
+    }
+
+    public void perderVida() {
+        if (vidas > 0) {
+            vidas--;
+        }
+    }
+
+    public abstract void mover();
+}
