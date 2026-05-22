feca1da115c1cf4c8d45c6d7f3713cd2b6aee5fd
brutom26@gmail.com <brutom26@gmail.com>
2026-05-22 16:42:08 -0300

    clase Alien.java completa

--- proyecto/src/spaceinvaders/Alien.java
+++ proyecto/src/spaceinvaders/Alien.java
@@ -1,19 +1,20 @@
 package spaceinvaders;
 
-import motor.Entidad;
-import java.awt.Graphics;
 
-public class Alien extends Entidad {
+import java.awt.Graphics;
+import motor.Enemigo;
+
+public class Alien extends Enemigo {
 
     public static final int CALAMAR = 0;
     public static final int CANGREJO = 1;
     public static final int PULPO = 2;
     private int tipo;
-
+    private boolean direccionDerecha = true; // Dirección inicial del movimiento
     private int FrameAnimacion;
 
     public Alien(int tipo, int x, int y) {
-        super(x, y, 32, 32);
+        super(x, y, 32, 32, 1.0f); // Tamaño y velocidad del alien
         this.tipo = tipo;
     }
 
@@ -31,9 +32,26 @@ public class Alien extends Entidad {
 
     public void actualizar() {
         // Lógica para actualizar la animación del alien
+        FrameAnimacion++;
+        if (FrameAnimacion >= 30) { // Cambia de frame cada 30 actualizaciones
+            FrameAnimacion = 0;
+        }
     }
 
-
+    public int obtenerPuntaje() {
+        // Retorna el puntaje basado en el tipo de alien
+        switch (tipo) {
+            case CALAMAR:
+                return 10;
+            case CANGREJO:
+                return 20;
+            case PULPO:
+                return 30;
+            default:
+                return 0;
+        }
+    }
+    
 
     @Override
     public void dibujar(Graphics g) {
