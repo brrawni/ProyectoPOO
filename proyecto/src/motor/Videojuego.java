e03e9fdb2f91f5031e97e43b1f05961484bcdac2
brutom26@gmail.com <brutom26@gmail.com>
2026-05-22 00:34:24 -0300

    Cree la clase abstracta VideojuegO

---
+++ proyecto/src/motor/Videojuego.java
@@ -0,0 +1,42 @@
+package motor;
+import java.awt.Graphics;
+
+public class Videojuego {
+    protected int nivelActual;
+    protected boolean enEjecucion;
+    protected int puntaje;
+
+    public Videojuego() {
+        this.nivelActual = 1;
+        this.enEjecucion = false;
+        this.puntaje = 0;
+    }
+
+    public void iniciar(){
+        this.enEjecucion = true;
+        this.puntaje = 0;
+        this.nivelActual = 1;
+    }
+
+    public void pausar(){
+        this.enEjecucion = false;
+    }
+
+    public void reanudar() {
+        this.enEjecucion = true;
+    }
+
+    public int obtenerPuntaje() {
+        return puntaje;
+    }
+
+    public void sumarPuntaje(int puntos) {
+        if(puntos > 0){
+            this.puntaje += puntos;
+        }
+    }
+
+    public abstract void actualizar();
+    public abstract void dibujar(Graphics g);
+    public abstract boolean verificarFinJuego();
+}
