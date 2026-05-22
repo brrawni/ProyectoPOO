9cd4420d3d4974bf96b4474e9b4a4c99991bb693
brutom26@gmail.com <brutom26@gmail.com>
2026-05-22 18:30:36 -0300

    modifique la clase Entidad.java

--- proyecto/src/spaceinvaders/ProyectilAlien.java
+++ proyecto/src/spaceinvaders/ProyectilAlien.java
@@ -4,30 +4,31 @@ import java.awt.Graphics;
 import motor.Proyectil;
 
 public class ProyectilAlien extends Proyectil {
-    private float velocidad = 5.0f; // Velocidad del proyectil hacia abajo
 
-    public ProyectilAlien(int x, int y, FormacionAlien formacion, Jugador jugador) {
-        super(x, y, 5, 10, 0, velocidad); // Tamaño y velocidad del proyectil
-        this.formacion = formacion;
+    private CanonJugador jugador;
+
+    public ProyectilAlien(int x, int y, CanonJugador jugador) {
+        super(x, y, 5, 10, 0, 5.0f);
         this.jugador = jugador;
     }
 
     @Override
     public void actualizar() {
-        y += dy; // Mueve el proyectil hacia abajo
-        if (y > 600) desactivar(); // Si sale de pantalla se destruye
+        y += dy;
+        if (y > 600) desactivar();
         verificarImpacto();
     }
 
+    @Override
     public void verificarImpacto() {
-        if (this.obtenerLimites().intersects(jugador.obtenerLimites())) {
-            jugador.recibirImpacto(); // Lógica para que el jugador reciba daño o pierda una vida
+        if (jugador != null && this.obtenerLimites().intersects(jugador.obtenerLimites())) {
+            jugador.perderVida();
             this.desactivar();
         }
     }
 
     @Override
     public void dibujar(Graphics g) {
-        g.fillRect(x, y, ancho, alto); // Ejemplo de un rectángulo representando al proyectil alienígena
+        g.fillRect(x, y, ancho, alto);
     }
 }
