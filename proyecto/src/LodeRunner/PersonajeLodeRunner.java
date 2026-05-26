ab07e3d8b9e41ba9c771898f1e6089b60f75ac41
tomasruiz23 <dayyraal@gmail.com>
2026-05-26 19:08:40 -0300

    Actualice Lode Runner

--- proyecto/src/LodeRunner/PersonajeLodeRunner.java
+++ proyecto/src/LodeRunner/PersonajeLodeRunner.java
@@ -18,6 +18,7 @@ public abstract class PersonajeLodeRunner extends Entidad{
         this.escenario = escenario;
     }
     public void aplicarGravedad(){
+        this.x = ((this.x + 16)/32)*32;
         this.y += 4; // Cae por gravedad
     }
     public int getX(){
@@ -38,6 +39,8 @@ public abstract class PersonajeLodeRunner extends Entidad{
 class Guardia extends PersonajeLodeRunner{
     private int temporizadorPatrulla = 0;
     private Oro oroGuardado;
+    private boolean persiguiendo;
+    private boolean heroeArriba;
 
     public Guardia(int x, int y, int ancho, int alto, Escenario escenario){
         super(x, y, ancho, alto, escenario);
@@ -45,27 +48,37 @@ class Guardia extends PersonajeLodeRunner{
     }
     @Override
     public boolean detectarColision() {
+        // 1. Olvidar si estaba en escalera o colgado de barra
         enEscalera = false;
         colgadoDeBarra = false;
-        boolean colision = true;
-        int filaAbajo = (this.y + this.alto) / 32;
-        int columnaAbajo = (this.x + this.ancho + 2) / 32;
-        int tipoBloque = escenario.obtenerTipoBloqueEn(filaAbajo, columnaAbajo);
-        if (tipoBloque == 0)
-            colision = false;
-        else{
-            colision = true;
-            if (tipoBloque == 3){
-                enEscalera = true;
-                direccion = 3;
-            }
-            else if (tipoBloque == 4){
-                colgadoDeBarra = true;
-                direccion = 3;
-            }
+        boolean hayPiso = true;
 
+        // 2. SENSOR DE CUERPO: ¿Estamos tocando una escalera o barra con el torso?
+        int filaCentro = (this.y + this.alto / 2 ) / 32;
+        int columnaCentro = (this.x + this.ancho / 2 ) / 32;
+        int bloqueCuerpo = escenario.obtenerTipoBloqueEn(filaCentro, columnaCentro);
+
+        if (bloqueCuerpo == 3) {
+            enEscalera = true;
+        } else if (bloqueCuerpo == 4) {
+            colgadoDeBarra = true;
         }
-        return colision;
+
+        // 3. SENSOR DE PIES: ¿Tenemos suelo firme debajo para no caer por gravedad?
+        int filaAbajo = (this.y + this.alto) / 32;
+        int bloquePies = escenario.obtenerTipoBloqueEn(filaAbajo, columnaCentro);
+
+        if (bloquePies == 0) {
+            hayPiso = false; // Hay aire, nos caemos
+        } else {
+            hayPiso = true; // Hay piso (ladrillo, escalera, etc.)
+            // Si el piso debajo de mis pies es la punta de una escalera, también cuenta
+            if (bloquePies == 3) {
+                enEscalera = true;
+            }
+        }
+
+        return hayPiso;
     }
     public boolean detectarColision(Entidad entidad){
         return new Rectangle(this.x, this.y, this.ancho, this.alto).intersects(new Rectangle(entidad.obtenerX(), entidad.obtenerY(), entidad.obtenerAncho(), entidad.obtenerAlto()));
@@ -79,24 +92,38 @@ class Guardia extends PersonajeLodeRunner{
         double base = (heroe.getX() + heroe.getAncho()/2) - (this.x + this.ancho/2);
         double altura = (heroe.getY() + heroe.getAlto()/2) - (this.y + this.alto/2);
         double hipotenusa = Math.sqrt(Math.pow(base, 2.0) + Math.pow(altura, 2.0));
-        if (hipotenusa < 320){ //El rango de vision de los guardias esta definido en 10 bloques, y cada bloque mide 32 pixeles de largo y ancho
+        if (hipotenusa < 64){ //El rango de vision de los guardias esta definido en 10 bloques, y cada bloque mide 32 pixeles de largo y ancho
+            persiguiendo = true;
             if (base > 0)
                 this.direccion = 1; //heroe a la derecha
             else if (base < 0)
                 this.direccion = 0; //heroe a la izquierda
             if (this.y > heroe.getY()){
-                if (enEscalera || colgadoDeBarra) //si encuentra soga o escalera, sube
+                if (enEscalera || colgadoDeBarra){ //si encuentra soga o escalera, sube
                     this.direccion = 2; //heroe esta arriba
+
+                }
             }
             else if (this.y < heroe.getY()){
-                if (enEscalera)
-                    this.direccion = 3; //heroe esta abajo
+                // Le damos "ojos" a la IA para mirar los pies
+                int filaPies = (this.y + this.alto) / 32;
+                int colCentro = (this.x + this.ancho / 2) / 32;
+                int bloqueAbajo = escenario.obtenerTipoBloqueEn(filaPies, colCentro);
+                int bloqueEnPies = escenario.obtenerTipoBloqueEn(filaPies + 1, colCentro);
+                // Solo le da la orden de bajar (3) si NO hay un ladrillo.
+                // Si hay un ladrillo sólido (1), esto da falso.
+                // Al dar falso, el guardia mantiene la dirección horizontal (0 o 1) y sigue de largo.
+                if (bloqueEnPies == 3 || bloqueAbajo == 3) {
+                    this.direccion = 3;
+                    return; //este return sirve para que en cada frame no se "Pise" la direccion actual del guardia
+                }
             }
             if (detectarColision(heroe)){
                 heroe.perderVida();
             }
         }
         else{
+            persiguiendo = false;
             moverAleatoriamente();
         }
     }
@@ -117,7 +144,6 @@ class Guardia extends PersonajeLodeRunner{
                     this.x -= 2;
                     if (tipoBloque == 3){
                         enEscalera = true;
-                        direccion = 2;
                     }
                     else{
                         enEscalera = false;
@@ -135,7 +161,6 @@ class Guardia extends PersonajeLodeRunner{
                     this.x += 2;
                     if (tipoBloque == 3){
                         enEscalera = true;
-                        direccion = 2;
                     }
                     else{
                         enEscalera = false;
@@ -148,14 +173,18 @@ class Guardia extends PersonajeLodeRunner{
                 break;
             case 2:
                 if (enEscalera){
+                    this.x = ((this.x + 16)/32)*32; //para que no desfase de la escalera
                     this.y -= 2;
                 }
                 break;
             case 3:
-                if (enEscalera || colgadoDeBarra)
+                if (enEscalera || colgadoDeBarra){
+                    this.x = ((this.x + 16)/32)*32;
                     this.y += 2;
-                else
+                }
+                else{
                     aplicarGravedad();
+                }
                 break;
         }
     }
@@ -171,7 +200,7 @@ class Guardia extends PersonajeLodeRunner{
     }
     
     public void robarOro(Oro oro){
-        if (oro != null && detectarColision(oro)){
+        if (oroGuardado == null && detectarColision(oro)){
             oroGuardado = oro;
             oroGuardado.esRecolectado(this, null);
         }
@@ -207,23 +236,36 @@ class Heroe extends PersonajeLodeRunner{
     }
     @Override
     public boolean detectarColision() {
-        boolean colision = true;
+        // 1. Olvidar si estaba en escalera o colgado de barra
+        enEscalera = false;
+        colgadoDeBarra = false;
+        boolean hayPiso = true;
+
+        // 2. SENSOR DE CUERPO: ¿Estamos tocando una escalera o barra con el torso?
+        int filaCentro = (this.y + this.alto / 2 ) / 32;
+        int columnaCentro = (this.x + this.ancho / 2 ) / 32;
+        int bloqueCuerpo = escenario.obtenerTipoBloqueEn(filaCentro, columnaCentro);
+
+        if (bloqueCuerpo == 3) {
+            enEscalera = true;
+        } else if (bloqueCuerpo == 4) {
+            colgadoDeBarra = true;
+        }
+
+        // 3. SENSOR DE PIES: ¿Tenemos suelo firme debajo para no caer por gravedad?
         int filaAbajo = (this.y + this.alto) / 32;
-        int columnaAbajo = (this.x + this.ancho + 2) / 32;
-        int tipoBloque = escenario.obtenerTipoBloqueEn(filaAbajo, columnaAbajo);
-        if (tipoBloque == 0)
-            colision = false;
-        else{
-            colision = true;
-            if (tipoBloque == 3){
+        int bloquePies = escenario.obtenerTipoBloqueEn(filaAbajo, columnaCentro);
+
+        if (bloquePies == 0) {
+            hayPiso = false; // Hay aire, nos caemos
+        } else {
+            hayPiso = true; // Hay piso (ladrillo, escalera, etc.)
+            // Si el piso debajo de mis pies es la punta de una escalera, también cuenta
+            if (bloquePies == 3) {
                 enEscalera = true;
             }
-            else if (tipoBloque == 4){
-                colgadoDeBarra = true;
-            }
-
         }
-        return colision;
+        return hayPiso;
     }
     @Override
     public void dibujar(Graphics2D g){
@@ -241,22 +283,20 @@ class Heroe extends PersonajeLodeRunner{
         oro.esRecolectado(null, this);
 
     }
+    @Override
     public void mover(){
-        //No usamos este metodo
-    }
-    public void mover(int direccion){
-        this.direccion = direccion;
+
         boolean tienePiso = detectarColision();
         if (!tienePiso && !enEscalera && !colgadoDeBarra){
             aplicarGravedad();
             return;
         }
         int columnaIzquierda, columnaDerecha, tipoBloque;
-        int filaCentro = (this.y + this.alto/2) / 32; //si el centro de gravedad del heroe choca contra un bloque
+        int filaCentro = (this.y + this.alto/2) / 32; //si el centro de gravedad del guardia choca contra un bloque
 
         switch (direccion){
             case 0:
-                columnaIzquierda = (this.x - 2) / 32; //anticipamos el siguiente paso del heroe
+                columnaIzquierda = (this.x - 2) / 32; //anticipamos el siguiente paso del guardia
                 tipoBloque = escenario.obtenerTipoBloqueEn(filaCentro, columnaIzquierda);
                 if (tipoBloque == 0 || tipoBloque == 3 || tipoBloque == 4){
                     this.x -= 2;
@@ -269,7 +309,7 @@ class Heroe extends PersonajeLodeRunner{
                     }
                 }
                 else{
-                    this.x -= 0; //no se puede atravesar una pared.
+                    this.x += 0; //no se puede atravesar la pared
                 }
                 break;
             case 1:
@@ -286,21 +326,47 @@ class Heroe extends PersonajeLodeRunner{
                     }
                 }
                 else{
-                    this.x -= 0; //no se puede atravesar la pared
+                    this.x += 0; //no se puede atravesar la pared
                 }
                 break;
             case 2:
                 if (enEscalera){
-                    this.y -= 2;
+                    this.x = ((this.x + 16)/32)*32; //para que no desfase de la escalera
+                    // PROTECCIÓN DE TECHO (CASCO)
+                    int filaCabeza = (this.y - 2) / 32;
+                    int colCentroAlineado = this.x / 32;
+                    int bloqueArriba = escenario.obtenerTipoBloqueEn(filaCabeza, colCentroAlineado);
+
+                    // Si arriba hay aire (0) o escalera (3), sube. Si hay ladrillo, choca y no hace nada.
+                    if (bloqueArriba != 1) {
+                        this.y -= 2;
+                    }
                 }
                 break;
             case 3:
-                if (enEscalera || colgadoDeBarra)
-                    this.y += 2;
+                if (enEscalera || colgadoDeBarra) {
+                    this.x = ((this.x + 16) / 32) * 32; // Alineación
+
+                    // PROTECCIÓN ANTI-ENTIERRO
+                    int filaSuelo = (this.y + this.alto + 2) / 32;
+                    int colSuelo = this.x / 32;
+                    int bloqueAbajo = escenario.obtenerTipoBloqueEn(filaSuelo, colSuelo);
+
+                    if (bloqueAbajo != 1) {
+                        this.y += 2; // Si no hay ladrillo, sigue bajando
+                    }
+                }
+                else
+                    aplicarGravedad();
+                break;
+            default:
                 break;
         }
     }
     public void perderVida(){
         vidas--;
+    }
+    public void setDireccion(int direccion){
+        super.direccion = direccion;
     }
 }
