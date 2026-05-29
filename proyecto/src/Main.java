b981fb786c343dc263e57268c5731781c529c226
tomasruiz23 <dayyraal@gmail.com>
2026-05-28 23:23:02 -0300

    Lode runner al 80%

--- proyecto/src/Main.java
+++ proyecto/src/Main.java
@@ -3,9 +3,9 @@ import LodeRunner.LodeRunnerMain;
 
 public class Main {
     public static void main(String[] args) {
+        LodeRunnerMain juego = new LodeRunnerMain();
+        juego.run();
         //SpaceInvaders juego = new SpaceInvaders();
         //juego.run();
-        SpaceInvaders juego = new SpaceInvaders();
-        juego.run();
     }
 }
