62a20b78c9672845adc13b61a49ad2f7bdea67c3
tomasruiz23 <dayyraal@gmail.com>
2026-06-01 13:27:47 -0300

    Corregi la carpeta de recursos

---
+++ proyecto/src/LodeRunner/GestorRecursos.java
@@ -0,0 +1,35 @@
+package LodeRunner;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class GestorRecursos {
+    private HashMap<String, BufferedImage> originalHeroe;
+    private HashMap<String, BufferedImage> imgGuardia;
+
+    public GestorRecursos(){
+        imgGuardia = new HashMap<>();
+        originalHeroe = new HashMap<>();
+        try{
+            BufferedImage hojaSprites = ImageIO.read(getClass().getResourceAsStream("/resources/img/loderunner/personajes.png"));
+            for (int i = 0; i < 3; i++){
+                //animaciones guardia corriendo
+                imgGuardia.put("corriendo" + (i + 1), hojaSprites.getSubimage(16*i, 3*16, 16, 16));
+                //animaciones guardia escalera
+                imgGuardia.put("escalera" + (i + 1), hojaSprites.getSubimage(16*i, 4*16, 16, 16));
+                //animaciones guardia cayendo
+                imgGuardia.put("cayendo" + (i + 1), hojaSprites.getSubimage(16*(i+4), 3*16, 16, 16));
+                //animaciones guardia barra
+                imgGuardia.put("barra" + (i + 1), hojaSprites.getSubimage(16*(i+4), 4*16, 16, 16));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void cargarSkin(String nombreSkin){
+
+    }
+}
