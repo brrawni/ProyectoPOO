5f96bb00575847b3789c4d2edd60d2cb0d197682
tomasruiz23 <dayyraal@gmail.com>
2026-06-04 01:33:13 -0300

    Mejore la IA de los guardias

--- proyecto/src/LodeRunner/LodeRunnerMain.java
+++ proyecto/src/LodeRunner/LodeRunnerMain.java
@@ -47,7 +47,7 @@ public class LodeRunnerMain extends Videojuego implements KeyListener{
         canvas.setFocusable(true);
         canvas.requestFocus();
         canvas.requestFocusInWindow();
-        nivelActual = 1;
+        nivelActual = 3;
         vidasHeroe = 5;
         config.cargar();
     }
