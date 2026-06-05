ebc3c4685440fa7cf0c60c65265e203706784ed6
brutom26@gmail.com <brutom26@gmail.com>
2026-06-04 22:31:46 -0300

    cambios

--- proyecto/src/spaceinvaders/GestorConfiguracionSpaceInvaders.java
+++ proyecto/src/spaceinvaders/GestorConfiguracionSpaceInvaders.java
@@ -39,7 +39,7 @@ public class GestorConfiguracionSpaceInvaders extends GestorConfiguracionBase {
         props.setProperty("teclaDisparo", String.valueOf(teclaDisparo));
         props.setProperty("skinNave", skinNave);
         props.setProperty("skinInvasores", skinInvasores);
-        props.setProperty("skinProyectil", skinProyectiles);
+        props.setProperty("skinProyectiles", skinProyectiles);
         props.setProperty("pistaMusical", pistaMusical);
         
         // El padre guarda sus variables y escribe todo en el disco
