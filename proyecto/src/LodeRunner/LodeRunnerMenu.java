81d382d088589d35cf11296bda1feaeb398e4764
tomasruiz23 <dayyraal@gmail.com>
2026-06-04 22:46:51 -0300

    Cree menu de Lode Runner

--- proyecto/src/LodeRunner/LodeRunnerMenu.java
+++ proyecto/src/LodeRunner/LodeRunnerMenu.java
@@ -1,4 +1,21 @@
 package LodeRunner;
 
-public class LodeRunnerMenu {
+import javax.swing.*;
+
+public class LodeRunnerMenu extends JFrame{
+    public LodeRunnerMenu() {
+        // Configuramos la ventana
+        setTitle("Lode Runner");
+        setSize(800, 600);
+        setResizable(false);
+        setLocationRelativeTo(null); // Centra la ventana
+
+        // DISPOSE_ON_CLOSE es clave para que al cerrar el juego,
+        // no se cierre también el Launcher de fondo.
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // Instanciamos y agregamos el gestor de los menús
+        GestorPantallas gestor = new GestorPantallas();
+        add(gestor);
+    }
 }
